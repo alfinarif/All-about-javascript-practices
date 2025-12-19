9d1c54e371cd0fbaefd71ba364c59911f083304f
alfinarif <cleavercode@gmail.com>
2025-12-19 06:19:35 -0800

    jspractices

---
+++ jsPractices/variable.js
@@ -0,0 +1,12 @@
+// console.log('5 + 10 =' + (5 + 10));
+// console.log('5 - 10 =' + (5 - 10));
+// console.log('5 * 10 =' + (5 * 10));
+// console.log('5 / 10 =' + (5 / 10));
+
+
+let x = 5;
+let y = 50;
+
+let sum  = (x + y);
+
+console.log(sum);
