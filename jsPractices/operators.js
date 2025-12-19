3b8ed1c730b3a156553742c13fd5a7b712ec3864
alfinarif <cleavercode@gmail.com>
2025-12-19 18:45:52 +0300

    operators

---
+++ jsPractices/operators.js
@@ -0,0 +1,28 @@
+// arrithmetic operaton
+let x = 18
+let y = 5
+
+let sum = x + y
+let sub = x - y
+let mul = x * y
+let div = x / y
+let mod = x % y
+
+// console.log(sum)
+// console.log(sub)
+// console.log(mul)
+// console.log(div)
+// console.log(mod)
+
+
+
+
+// assignment operator
+
+// console.log(x += y)
+// console.log(x -= y)
+// console.log(x *= y)
+// console.log(x /= y)
+// console.log(x %= y)
+
+
