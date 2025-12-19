cba878608bff258a91c6f5b9da106d5f28298cd8
alfinarif <cleavercode@gmail.com>
2025-12-19 06:26:31 -0800

    data type

---
+++ jsPractices/dataType.js
@@ -0,0 +1,17 @@
+
+let a = 'Hello'
+let b = 20
+let c = 20.5
+let d = true
+let e = undefined
+let u
+let n = null
+
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof u)
+console.log(typeof n)
