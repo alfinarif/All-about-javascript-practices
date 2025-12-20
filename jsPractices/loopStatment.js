5b5a5e50ac059c05251e6559a9da5ffc21bf82e3
alfinarif <cleavercode@gmail.com>
2025-12-20 17:51:03 +0300

    loop

---
+++ jsPractices/loopStatment.js
@@ -0,0 +1,66 @@
+// All about loop
+
+// for (let i = 1; i <= 100; i++){
+//     console.log(i);
+// }
+
+
+// another example
+// let sum = 0;
+
+// for (let i = 1; i <= 5; i++){
+//     sum += i;
+// }
+
+// console.log(sum);
+
+
+
+
+// while loop
+
+// let i = 1;
+// while (i <= 100){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let total = 0;
+// let x = 1;
+// while(x <= 5){
+//     total += x;
+//     x++;
+// }
+// console.log(total);
+
+
+
+
+// do while loop
+
+// this example for while loop to understand to do while loop
+// const isLoop = false;
+// while(isLoop){
+//     console.log("This loop will never run");
+// }
+
+// do while loop will run at least once
+// do {
+//     console.log("It will run at least once");
+// }while(isLoop);
+
+
+
+
+// for in loop
+const cname = "Mohammad Arif";
+
+// for (let i in cname){
+//     console.log(cname[i]);
+// }
+
+// for of loop
+for (let v of cname){
+    console.log(v);
+}
